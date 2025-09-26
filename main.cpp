d94eee1c2f385f75a301985f5dd917dd254180c9
xardb <vlad96mir@bk.ru>
2025-09-26 13:22:27 +0300

    2. Печатает введённую строку в цикле и выходит по Ctrl+D 3. Добавим команду для выхода (`\q`) 4. Добавим историю введённых команд и её сохранения в файл `~/.kubsh_history` 5. Добавим команду echo (`echo "123"` выводит `123`)

--- main.cpp
+++ main.cpp
@@ -7,28 +7,28 @@ using namespace std;
 int main() {
     system("chcp 65001");
     string input;
-    string history_file = "kubsh_history.txt";
-    ofstream write_file(history_file, ios::app);
+//    string history_file = "kubsh_history.txt";
+//    ofstream write_file(history_file, ios::app);
 
     cout << "мой шелл :)" << endl;
 
-    while (true) {
+//    while (true) {
         cout << "₽ ";
         if (!getline(cin, input)) {
             break; // Ctrl+D
         }
 
-        if (input.empty()) continue;
-        write_file << input << endl;
-        write_file.flush();
-        if (input == "\\q") break;
-
-        if (input.find("echo ") == 0) {
-            cout << input.substr(5) << endl;
-        } else {
+//        if (input.empty()) continue;
+//        write_file << input << endl;
+//        write_file.flush();
+//        if (input == "\\q") break;
+//
+//        if (input.find("echo ") == 0) {
+//            cout << input.substr(5) << endl;
+//        } else {
             cout << input << endl;
-        }
+//        }
     }
-    write_file.close();
-    cout << "Выход" << endl;
+//    write_file.close();
+//    cout << "Выход" << endl;
 }
